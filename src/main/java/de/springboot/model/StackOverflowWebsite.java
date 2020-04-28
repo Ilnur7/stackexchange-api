95ca9b432f0721afd1f9b01f8c3dd674658b8a07
Ilnur7 <ilnur_gaysin@amil.ru>
2020-04-29 00:55:30 +0300

    Data from List

---
+++ src/main/java/de/springboot/model/StackOverflowWebsite.java
@@ -0,0 +1,21 @@
+package de.springboot.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StackOverflowWebsite {
+
+    @Id
+    private String id;
+    private String website;
+    private String iconImageUrl;
+    private String title;
+    private String description;
+}
